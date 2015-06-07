44824886728dff2e3fc885e82b4839840276a2c7
あまみや ゆうこ <1@4321.io>
2015-06-07 21:58:56 +0800

    1

--- Qqhru/Controllers/TeacherController.cs
+++ Qqhru/Controllers/TeacherController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Qqhru.Models;
 
 namespace Qqhru.Controllers
 {
@@ -28,6 +29,24 @@ namespace Qqhru.Controllers
             var user = DB.Users.Find(id);
             if (user == null) return Msg("没有找到该教师");
             return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, string Name, Sex Sex, string Profession, int GroupID, Title Title)
+        {
+            var user = DB.Users.Find(id);
+            user.Name = Name;
+            user.Sex = Sex;
+            user.Profession = Profession;
+            user.GroupID = GroupID;
+            user.Title = Title;
+            DB.SaveChanges();
+            return Redirect("/Teacher/Show/" + id);
+        }
+
+        public ActionResult ResearchUpload(int id)
+        {
+
         }
     }
 }
